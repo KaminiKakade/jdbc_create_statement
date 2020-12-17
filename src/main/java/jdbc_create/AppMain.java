ebe6e9dcab10e447159cd721ddd92dea685110c7
DELL <DELL@DESKTOP-SCNOICP>
2020-12-17 13:03:15 +0530

    jdbc CRUD: using create statement

--- src/main/java/jdbc_create/AppMain.java
+++ src/main/java/jdbc_create/AppMain.java
@@ -35,10 +35,14 @@ public class AppMain {
 			String q1 = "insert into emp values(11,'Kamini',25000)";
 			String q2 = "insert into emp values(12,'Mayuri',35000)";
 			String q3 = "insert into emp values(13,'Kajal',28000)";
+			String q4 = "insert into emp values(14,'Sanem',18000)";
+
 			// execute a query to insert records
 			statement.executeUpdate(q1);
 			statement.executeUpdate(q2);
 			statement.executeUpdate(q3);
+			statement.executeUpdate(q4);
+
 			System.out.println("records inserted successfully...");
 			// STEP 5: Extract data from result set
 			String sql = "select * from emp";
@@ -56,6 +60,48 @@ public class AppMain {
 			}
 			System.out.println("records retrieved successfully...");
 
+			
+			String queryU="Update emp SET eSal=38000 where eId=11";
+			statement.executeUpdate(queryU);
+			System.out.println("records updated successfully..");
+			 
+			System.out.println("records after updation...>>>");
+			//we will extract all records to see updated records
+			queryU="select * from emp";
+			 rs=statement.executeQuery(queryU);
+			 while (rs.next()) {
+					// Retrieve by column name
+					int id = rs.getInt("eId");
+					String name = rs.getString("eName");
+					int salary = rs.getInt("eSal");
+					// Display values
+					System.out.print("ID: " + id);
+					System.out.print(", name: " + name);
+					System.out.println(", salary: " + salary);
+
+				}
+			 query="delete from emp where eId=12";
+			 statement.executeUpdate(query);
+			 System.out.println("record deleted successffully..");
+			//we will extract all records to see deleted records
+			 System.out.println("records after deletion ..>>>");
+			 rs=statement.executeQuery(queryU);
+			 while (rs.next()) {
+					// Retrieve by column name
+					int id = rs.getInt("eId");
+					String name = rs.getString("eName");
+					int salary = rs.getInt("eSal");
+					// Display values
+					System.out.print("ID: " + id);
+					System.out.print(", name: " + name);
+					System.out.println(", salary: " + salary);
+
+				}
+			 
+			 
+			 
+			 
+			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} finally {
